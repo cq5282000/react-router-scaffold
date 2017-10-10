5a3b873ba528b20d1bed4bbd3a649b37b0caa0b5
chenqu <chenqu@chenqudeMacBook-Pro-2.local>
2017-10-10 11:40:45 +0800

    add

--- src/entry/index.js
+++ src/entry/index.js
@@ -1,46 +1,15 @@
 /**
  * Created by chenqu on 2017/9/29.
  */
-import React, { Component, Children, createElement } from 'react';
-import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import { Router, hashHistory } from 'react-router';
-// import DetailContainer from '../containers/DetailContainer';
 import RouterContainer from '../containers/base/RouterContainer';
+import render from '../lib/createRender';
 
-// hack around https://github.com/gaearon/react-hot-boilerplate/pull/61#issuecomment-211504531
-Router.prototype.componentWillReceiveProps = (nextProps) => {
-    const components = [];
-
-    const grabComponents = (element) => {
-        // This only works for JSX routes, adjust accordingly for plain JS config
-        if (element.props && element.props.component) {
-            components.push(element.props.component);
-        }
-        if (element.props && element.props.children) {
-            Children.forEach(element.props.children, grabComponents);
-        }
-    };
-
-    grabComponents(nextProps.routes || nextProps.children);
-    components.forEach(createElement); // force patching
-};
-
-render(
-    <AppContainer>
-        <RouterContainer/>
-    </AppContainer>,
-    document.getElementById('app'),
-);
+render(RouterContainer);
 
 if (module.hot) {
     module.hot.accept('../containers/base/RouterContainer.js', () => {
         const NextApp = require('../containers/base/RouterContainer.js').default;
-        render(
-            <AppContainer>
-                <NextApp/>
-            </AppContainer>,
-            document.getElementById('app'),
-        );
+        render(RouterContainer);
     });
 }
--- src/lib/createRender.js
+++ src/lib/createRender.js
@@ -1,9 +1,27 @@
 /**
  * Created by chenqu on 2017/10/10.
  */
-import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
+import { Router, hashHistory } from 'react-router';
+import React, { Component, Children, createElement } from 'react';
+// hack around https://github.com/gaearon/react-hot-boilerplate/pull/61#issuecomment-211504531
+Router.prototype.componentWillReceiveProps = (nextProps) => {
+    const components = [];
+
+    const grabComponents = (element) => {
+        // This only works for JSX routes, adjust accordingly for plain JS config
+        if (element.props && element.props.component) {
+            components.push(element.props.component);
+        }
+        if (element.props && element.props.children) {
+            Children.forEach(element.props.children, grabComponents);
+        }
+    };
+
+    grabComponents(nextProps.routes || nextProps.children);
+    components.forEach(createElement); // force patching
+};
 
 export default (Entry) => {
     return render(
@@ -12,4 +30,4 @@ export default (Entry) => {
         </AppContainer>,
         document.getElementById('app'),
     );
-}
+};
